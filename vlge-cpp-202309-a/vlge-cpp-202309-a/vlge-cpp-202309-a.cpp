0b5e7ef183889e0ef6d8fc18b820364a109b6186
hoanglongdummy <104409899+hoanglongplanner@users.noreply.github.com>
2023-09-06 00:01:40 +0700

    Create vlge-cpp-202309-a
    Create VS2022 CMake CPP project:
    - vlge-cpp-202309-a

---
+++ vlge-cpp-202309-a/vlge-cpp-202309-a/vlge-cpp-202309-a.cpp
@@ -0,0 +1,11 @@
+﻿// vlge-cpp-202309-a.cpp : Defines the entry point for the application.
+//
+
+#include "vlge-cpp-202309-a.h"
+
+using namespace std;
+
+int main() {
+	cout << "Hello CMake." << endl;
+	return 0;
+}
