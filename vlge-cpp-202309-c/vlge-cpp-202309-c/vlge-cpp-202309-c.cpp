769d3772cd6a3b0bc7c0528b8a76cde87dce055d
hoanglongdummy <104409899+hoanglongplanner@users.noreply.github.com>
2023-09-17 18:23:35 +0700

    Update Stuffs

--- vlge-cpp-202309-c/vlge-cpp-202309-c/vlge-cpp-202309-c.cpp
+++ vlge-cpp-202309-c/vlge-cpp-202309-c/vlge-cpp-202309-c.cpp
@@ -1,12 +1,28 @@
 ﻿// vlge-cpp-202309-c.cpp : Defines the entry point for the application.
 //
 
+#include <iostream>
 #include "vlge-cpp-202309-c.h"
 
 using namespace std;
 
-int main()
-{
-	cout << "Hello CMake." << endl;
+//TODO: How to make this thing useful for main
+struct KExitCode {
+public:
+	const int K_Success() {
+		std::cout << "Program exit success." << std::endl;
+		return 0;
+	}
+	const int K_Fail() {
+		std::cout << "Program exit fail. Please Check." << std::endl;
+		std::cout << "Export log." << std::endl;
+		return 1;
+	}
+};
+
+int main(int argc, char* argv[]) {
+	const int K_ScreenWidth = 800;
+	const int K_ScreenHeight = 600;
+
 	return 0;
 }
