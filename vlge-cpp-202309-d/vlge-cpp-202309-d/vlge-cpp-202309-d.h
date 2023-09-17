2e3a73b03ac1752ff499d314bcf4df53f6921a9c
hoanglongdummy <104409899+hoanglongplanner@users.noreply.github.com>
2023-09-17 21:05:48 +0700

    Update Stuffs

--- vlge-cpp-202309-d/vlge-cpp-202309-d/vlge-cpp-202309-d.h
+++
@@ -1,8 +0,0 @@
-﻿// vlge-cpp-202309-d.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
