249917358d360834557da2b40ba086ab253988a7
hoanglongdummy <104409899+hoanglongplanner@users.noreply.github.com>
2023-09-17 17:20:37 +0700

    Init Project vlge-cpp-202309-c
    vlge-cpp-202309-c

---
+++ vlge-cpp-202309-c/vlge-cpp-202309-c/vlge-cpp-202309-c.cpp
@@ -0,0 +1,12 @@
+﻿// vlge-cpp-202309-c.cpp : Defines the entry point for the application.
+//
+
+#include "vlge-cpp-202309-c.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
