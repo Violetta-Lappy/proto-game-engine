b3c924bcced5bc54b27ce26f730d1885a5e7a24b
hoanglongdummy <104409899+hoanglongplanner@users.noreply.github.com>
2023-09-17 20:44:44 +0700

    Update Stuffs

---
+++ vlge-cpp-202309-d/vlge-cpp-202309-d/core_basic_window.cpp
@@ -0,0 +1,37 @@
+#include "raylib.h"
+
+#if defined(PLATFORM_WEB)
+#include <emscripten/emscripten.h>
+#endif
+
+const int K_ScreenWidth = 800;
+const int K_ScreenHeight = 600;
+
+int main(int argc, int* argv[])
+{
+	InitWindow(K_ScreenWidth, K_ScreenHeight, "raylib [core] example - basic window");
+
+#if defined(PLATFORM_WEB)
+	emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+#else
+	SetTargetFPS(60);   // Set our game to run at 60 frames-per-second	
+
+	// Main game loop
+	while (!WindowShouldClose())    // Detect window close button or ESC key
+	{
+		BeginDrawing();
+
+		ClearBackground(RAYWHITE);
+
+		DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+		EndDrawing();
+	}
+#endif
+
+	// Close window and OpenGL context
+	CloseWindow();
+
+	// return success
+	return 0;
+}
---
+++ vlge-cpp-202309-d/vlge-cpp-202309-d/vlge-cpp-202309-d.cpp
@@ -0,0 +1,12 @@
+﻿// vlge-cpp-202309-d.cpp : Defines the entry point for the application.
+//
+
+#include "vlge-cpp-202309-d.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
