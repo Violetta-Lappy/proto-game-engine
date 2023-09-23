a4bc7761bfccd45b6a07f0ae7499c09790f565bf
hoanglongdummy <104409899+hoanglongplanner@users.noreply.github.com>
2023-09-23 17:42:01 +0700

    Update InputManager.h

--- vlge-cpp-202309-d/vlge-cpp-202309-d/internal/include/ge-user/InputManager.h
+++ vlge-cpp-202309-d/vlge-cpp-202309-d/internal/include/ge-user/InputManager.h
@@ -1,5 +1,10 @@
 #pragma once
 
+//Work just like Unity New Input System and Unreal Enhanced Input
+//User-oriented context that suitable for their game
+
+//Universal name is Input Manager, don't reinvent stupid stuff
+
 class InputContext {
 
 };
